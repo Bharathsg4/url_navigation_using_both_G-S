0fe9b9b9ff6a134da39019e4cb2faaebd0f2055b
Bharathsg4 <bharathsg119@gmail.com>
2023-03-15 20:13:57 +0530

    first commit

---
+++ mi/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rohit(request):
+    return render(request,'rohit.html')
+
+def surya(request):
+    return render(request,'surya.html')
